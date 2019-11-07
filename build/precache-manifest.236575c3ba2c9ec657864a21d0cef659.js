5ec973004eebf6529047c693c62cafd52abdaf5a
hochan222 <201502766@hufs.ac.kr>
2019-11-07 19:36:14 +0900

    build

---
+++ build/precache-manifest.236575c3ba2c9ec657864a21d0cef659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dde4ef13238d866bda67ea4489d1fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c2cccadf83625971a31",
+    "url": "/static/css/2.a1297011.chunk.css"
+  },
+  {
+    "revision": "90d03153d0245d71cd36",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9c2cccadf83625971a31",
+    "url": "/static/js/2.21fe5be9.chunk.js"
+  },
+  {
+    "revision": "90d03153d0245d71cd36",
+    "url": "/static/js/main.1870942f.chunk.js"
+  },
+  {
+    "revision": "c619ad56732c884b7a32",
+    "url": "/static/js/runtime~main.892f92b1.js"
+  }
+]);
